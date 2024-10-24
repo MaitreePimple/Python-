9faeb857f0391b5c8bc7ad22e71bebe01e161d41
Maitree Pimple <89665415+MaitreePimple@users.noreply.github.com>
2024-10-25 03:08:58 +0530

    Add files via upload

---
+++ #BASIC5Python Casting.py
@@ -0,0 +1,17 @@
+# Python Casting
+#INT
+#x = int(1) 1
+#y = int(2.8) 2
+#z = int("3") 3
+#print(x,y,z)
+#FLOAT 
+#x = float(1) 1.0
+#y = float(2.8) 2.8
+#z = float("3") 3.0
+#print(x,y,z)
+#STRING 
+#w = str("s1") s1
+#x = str(1) 1
+#y = str(2.8) 2.8
+#z = str("3") 3
+#print(w,x,y,z)
---
+++ #BASICS4Python Numbers.py
@@ -0,0 +1,21 @@
+#Python Numbers
+#x=1
+#y=2.5
+#z=1j
+#print(type(x)) <class 'int'>
+#print(type(y)) <class 'float'>
+#print(type(z)) <class 'complex'>
+
+#Type Conversion
+#x=1
+#y=2.5
+#z=1j
+#a = float(x)
+#b = int(y)
+#c = complex (y)
+#print(a , b, c) 1.0,2,(2.5+0j)
+
+#Random Number
+
+#import random
+#print(random.randrange(1,10)) 9
