85f72b680f3b1efd97153347e7eddb561c292e74
Maitree Pimple <89665415+MaitreePimple@users.noreply.github.com>
2024-10-31 03:06:46 +0530

    Add files via upload

---
+++ #BASICS7Python Booleans.py
@@ -0,0 +1,58 @@
+#PYTHON BOOLEANS
+#EXAMPLES
+#print(10>9) True
+#print(10==9) False
+#print(10<9) False
+
+#a = 200
+#b = 33
+#if b>a:
+ #   print("b is greater than a")
+#else:
+ #   print("b is not greater than a") b is not greater than a
+
+#EVALUATE VALUES AND VARIABLES 
+#print(bool("Hello")) True
+#print(bool(15)) True 
+
+#x="Hello"
+#y=15
+#print(bool("Hello")) True
+#print(bool(15)) True 
+
+#Most Values are True
+#print (bool("abc"))True
+#print (bool(123)) True
+#print (bool(["apple", "cherry", "banana"])) True
+
+#Some Values are False
+#print(bool(False))
+#print(bool(None))
+#print(bool(0))
+#print(bool(""))
+#print(bool(()))
+#print(bool([]))
+#print(bool({}))
+
+#class myclass():
+ # def __len__(self):
+  #  return 0
+#myobj = myclass()
+#print(bool(myobj)) False
+
+#Functions can Return a Boolean
+#def myFunction():
+ #   return True 
+
+#print(myFunction()) True 
+
+#def myFunction():
+ #   return True 
+
+#if myFunction():
+#    print("Yes!")
+#else:
+#    print("NO!") YES!
+
+#x = 200
+#print(isinstance(x, int)) True 
